04ddf9142bd2495979b4a5d57e0551791bd2b294
Bheemalinga <kummarabheema@gmail.com>
2023-09-12 00:18:57 +0530

    Documentation Optimization

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const express = require('express'); // importing express
 // importing path
 const path = require('path'); // importing path for setting views directory
 const port = 8080; // port number
-const app = express(); // creating express instance
-const db = require('./config/db'); // importing database
-const Contact = require('./models/contact'); // importing model
-const contactRouter = require('./routes/contact'); // importing router
+const app = express(); // creating express instance, so that we can use express methods
+const db = require('./config/db'); // importing database,to connect to the database
+const Contact = require('./models/contact'); // importing model, to create schema
+const contactRouter = require('./routes/contact'); // importing router, to use router middleware for routing
+// middleware is used to convert the data into json format
 
-app.set('view engine', 'ejs'); // setting view engine
+app.set('view engine', 'ejs'); // setting view engine, for rendering ejs files
 app.set('views', './views'); // setting views directory
 
-app.use(express.urlencoded()); // using express.urlencoded middleware
-app.use('/',contactRouter); // using contactRouter middleware
+app.use(express.urlencoded()); // using express.urlencoded middleware, to convert the data into json format
+app.use('/',contactRouter); // using contactRouter middleware for routing inorder to avoid routing in index.js
 
-app.get('/', function(req, res){ // this is get request
-    return res.render('contact') // rendering contact.ejs
+app.get('/', function(req, res){ // this is get request for home page
+    return res.render('contact') // rendering contact.ejs file
 })
 
 app.listen(port, function(err){ // listening on port
--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,11 @@
+
+// In an MVC framework, the model represents the data layer.
+// While representing the application's data and business logic.
+// It's responsible for interacting with the database and preparing data to be displayed in the view.
+
 const mongoose = require('mongoose'); // importing mongoose
 
-const contactSchema = new mongoose.Schema({ // creating schema
+const contactSchema = new mongoose.Schema({ // creating schema for contact list app
     name : {
         type: String,
         required: true
